a4dc53589c41041d8d51e5de85a796d7a65df661
Dharamveer Saw <dharamveersaw32@gmail.com>
2023-10-19 07:18:41 +0530

    Created DAO for Login and Sign Up

---
+++ src/main/java/com/tonevellah/demofx1/dao/JdbcConnection.java
@@ -0,0 +1,16 @@
+package com.tonevellah.demofx1.dao;
+
+import java.sql.*;
+
+public class JdbcConnection {
+    Connection connection = null;
+    public Connection con() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/typerush", "root", "anappleaday.?20");
+        } catch(Exception e){
+            System.out.println("Connection not established.");
+            System.out.println(e);
+        }
+        return connection;
+    }
+}
